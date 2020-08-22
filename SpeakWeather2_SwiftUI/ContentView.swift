fe2f88bf8303540b201c8ea8e1254f9379456994
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-08-22 17:50:54 -0400

    Initial Commit

---
+++ SpeakWeather2_SwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SpeakWeather2_SwiftUI
+//
+//  Created by Alphonso Sensley II on 8/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpeakWeather2_SwiftUI/SpeakWeather2_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakWeather2_SwiftUIApp.swift
+//  SpeakWeather2_SwiftUI
+//
+//  Created by Alphonso Sensley II on 8/22/20.
+//
+
+import SwiftUI
+
+@main
+struct SpeakWeather2_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
